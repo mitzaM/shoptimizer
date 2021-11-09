374d1b3a78e0ce3fab3b1e6bcfc6b9e18e1ef9eb
Mihai Man <mihai.man@ly.st>
2021-11-09 17:11:03 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='shoptimizer_api',
+    version='1.0',
+    packages=['shoptimizer_api'],
+    zip_safe=False,
+    install_requires=[
+        'flask==1.1.1',
+        'requests==2.22.0',
+        'absl-py==0.9.0',
+        'mecab-python3==0.996.5',
+        'jaconv==0.2.4',
+    ],
+)

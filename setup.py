90fff34ec15cba19f4118b3e0ed389f462a405c2
Mihai Man <mihai.man@ly.st>
2021-11-09 17:34:29 +0200

    update requests version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     zip_safe=False,
     install_requires=[
         'flask==1.1.1',
-        'requests==2.22.0',
+        'requests>=2.22.0',
         'absl-py==0.9.0',
         'mecab-python3==0.996.5',
         'jaconv==0.2.4',
